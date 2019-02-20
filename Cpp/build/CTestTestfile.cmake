53135afe980cbc59ac95953bb64bf68b244a15f8
mmousaei <m.mousaei@gmail.com>
2019-02-20 18:37:22 -0500

    build removed

--- Cpp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mohammad/Documents/FaultDetection_Dataset/Cpp
-# Build directory: /home/mohammad/Documents/FaultDetection_Dataset/Cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
