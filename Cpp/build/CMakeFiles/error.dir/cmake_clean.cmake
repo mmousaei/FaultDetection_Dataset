53f9ce8086c77fe030eddbb0e372019aa7d486e8
mmousaei <m.mousaei@gmail.com>
2019-02-20 18:35:16 -0500

    cpp version tested on sample csv

---
+++ Cpp/build/CMakeFiles/error.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/error.dir/src/error.cpp.o"
+  "liberror.pdb"
+  "liberror.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/error.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Cpp/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohammad/Documents/FaultDetection_Dataset/Cpp
+# Build directory: /home/mohammad/Documents/FaultDetection_Dataset/Cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
